a3427cb4490d832e54351dcbc753e35180d39658
John McIver <john.mciver.iii@gmail.com>
2023-08-24 21:01:04 -0600

    Fix freeze_bits handling in handleMetadata

--- llvm_util/llvm2alive.cpp
+++ llvm_util/llvm2alive.cpp
@@ -1253,7 +1253,6 @@ public:
         break;
 
       // non-relevant for correctness
-      case LLVMContext::MD_freeze_bits:
       case LLVMContext::MD_loop:
       case LLVMContext::MD_nosanitize:
       case LLVMContext::MD_prof:
