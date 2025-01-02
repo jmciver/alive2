544622746a185c272dc5566d3dd46c17b8d3db5d
John McIver <john.mciver.iii@gmail.com>
2025-01-02 11:25:29 -0700

    Revert "Add support for `samesign` flag (#1098)"
    This reverts commit faf59b5d111b06c8edb87d073412484d59fc7b59.

--- ir/instr.h
+++ ir/instr.h
@@ -427,20 +427,17 @@ public:
     PROVENANCE, // compare pointer provenance & offsets
     OFFSETONLY // cmp ofs only. meaningful only when ptrs are based on same obj
   };
-  enum Flags { None = 0, SameSign = 1 << 0 };
 
 private:
   Value *a, *b;
   std::string cond_name;
   Cond cond;
-  unsigned flags;
   bool defined;
   PtrCmpMode pcmode = INTEGRAL;
   smt::expr cond_var() const;
 
 public:
-  ICmp(Type &type, std::string &&name, Cond cond, Value &a, Value &b,
-       unsigned flags = None);
+  ICmp(Type &type, std::string &&name, Cond cond, Value &a, Value &b);
 
   bool isPtrCmp() const;
   PtrCmpMode getPtrCmpMode() const { return pcmode; }
